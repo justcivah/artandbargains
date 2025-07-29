2dd596c60cd65e4acb02562dc47b3428c91e3f35
justcivah <me@giovanninovati.com>
2025-07-29 17:27:55 +0200

    Added dynamic proxy url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/sitemap.xml': 'https://backend-a2jy.onrender.com',
-      '/robots.txt': 'https://backend-a2jy.onrender.com'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = env.VITE_API_URL;
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/sitemap.xml': API_URL,
+        '/robots.txt': API_URL
+      }
     }
   }
-})
+});
