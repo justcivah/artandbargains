62fe689dcd48db8635860ec95fc72d5250200a2d
justcivah <me@giovanninovati.com>
2025-07-29 18:09:36 +0200

    Reverted code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  const API_URL = env.VITE_API_URL;
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/sitemap.xml': API_URL,
-        '/robots.txt': API_URL
-      }
-    }
-  }
-});
+export default defineConfig({
+  plugins: [react()],
+})
